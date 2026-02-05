dbcb6c16b61c69953bd02f4bdb39f19975de6f81
Malini <Siranjeevi9597536779@gmail.com>
2026-02-05 15:27:39 +0530

    changed backend url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Use environment variable or fallback to the deployed backend URL
-const API_URL = process.env.REACT_APP_API_URL || 'https://backend-crm-4.onrender.com/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://backend-crm-2-l5en.onrender.com/api';
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true,
